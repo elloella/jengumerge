27b6c55a8244c5f53f845d6e4fc49080e964bdb0
Ella Chambers <33899496+elloella@users.noreply.github.com>
2019-04-21 15:23:36 +0100

    Second Graph Added

--- js/script.js
+++ js/script.js
@@ -1,49 +1,78 @@
 //Setting up the chart.js
-const CHART = document.getElementById('myChartBS');
-console.log(CHART);
-let lineChart = new Chart(CHART, {
-type:'line',
+var chart_labels = ['10/3', '11/3', '12/3', '13/3', '14/3', '15/3', '16/3'];
+var sugar_dataset = [5, 11, 3, 6, 2, , ];
+var ctx =
+document.getElementById('myChart');
+var config = {
+  type:'line',
+  data: {
+    labels: chart_labels, //Setting labels for xAxes
+    datasets: [{
+      label: 'Blood Sugar',
+      lineTension: 0,
+      fill: false,
+      borderColor: 'rgb(110, 143, 197)', //Setting border colour for graph
+      data: sugar_dataset,
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    scales: {
+      yAxes: [{
+        gridLines: {
+          display: false ,
+          color: 'rgb(110, 143, 197)'
 
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Blood Sugar (mmol/l)' //Adding label for yAxes
+        },
 
-data: {
-  labels: ['3/3', '4/3', '5/3', '6/3', '7/3', '8/3', '9/3'], //Setting labels for xAxes
-  datasets: [{
-    label: 'Blood Sugar',
-    lineTension: 0,
-    fill: false,
-    borderColor: 'rgb(110, 143, 197)', //Setting border colour for graph
-    data: [5, 11, 3, 6, 2, 16, 7]
-  }]
-},
-options: {
-  scales: {
-    yAxes: [{
-      scaleLabel: {
-        display: true,
-        labelString: 'Blood Sugar (mmol/l)' //Adding label for yAxes
-      },
-    }],
+      }],
 
-    xAxes: [ {
-      scaleLabel: {
-        display: true,
-        labelString: 'Date' //Setting label for xAxes
-      }
-    } ]
+      xAxes: [ {
+        gridLines: {
+          display: false ,
+          color: 'rgb(110, 143, 197)'
+
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Date' //Setting label for xAxes
+
+        }
+      } ]
+    }
   }
-}
+};
+var myChart_chart = new Chart(ctx, config);
+$("#right").click(function() {
+  var data = myChart_chart.config.data;
+  data.datasets[0].data = sugar_dataset;
+  data.labels = chart_labels;
+  myChart_chart.update();
 });
 
-
+$("#left").click(function() {
+  var chart_labels = ['3/3', '4/3', '5/3', '6/3', '7/3', '8/3', '9/3'];
+  var sugar_dataset = [10, 3, 17, 11, 13, 5, 7];
+  var data = myChart_chart.config.data;
+  data.datasets[0].data = sugar_dataset;
+  data.labels = chart_labels;
+  myChart_chart.update();
+});
 
 Chart.defaults.global.defaultFontColor='#253D5B'; //Setting the font colour of graph
 
 window.onload = function() { //Setting up copyright logo
-currentYear();
+  currentYear();
 };
 
 function currentYear(){
-const date = new Date();
-const autoDate = document.querySelector('#autoDate');
-autoDate.textContent = date.getFullYear();
+  const date = new Date();
+  const autoDate = document.querySelector('#autoDate');
+  autoDate.textContent = date.getFullYear();
 };
