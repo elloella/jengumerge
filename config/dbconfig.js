d3d6b1bfc43e107ad6f7daec642ba4d4f7ea7f28
Christopher Booth <christopher.booth@plymouth.ac.uk>
2019-05-31 11:31:12 +0100

    updates to models, ejs files, and scripts relating

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,3 +1,4 @@
 module.exports = {
-  MongoURI: 'mongodb+srv://ello_ella98:S0Uth23413@cluster0-supx3.mongodb.net/test?retryWrites=true' //Setting up the link with database
+  //MongoURI: 'mongodb+srv://ello_ella98:S0Uth23413@cluster0-supx3.mongodb.net/test?retryWrites=true' //Setting up the link with database
+  MongoURI: 'mongodb://localhost:27017/jenguMaster'
 }
--- models/AddBPStats.js
+++ models/AddBPStats.js
@@ -1,40 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const AddBPStatsSchema = new mongoose.Schema({ //Setting up the backend form for the addition of data
-  timeStamp:{
-      type: Date,
-      default: Date.now
-  },
-  date:{
-    type: String,
-    required: true
-  },
-  breakfast: {
-      type: Number,
-      required: true
-  },
-  lunch: {
-      type: Number,
-      required: true
-  },
-  dinner: {
-      type: Number,
-      required: true
-  },
-  night: {
-      type: Number,
-      required: true
-  },
-  notes: {
+const AddBPStatsSchema = new mongoose.Schema({ 
+    timeStamp:{
+        type: Date,
+        default: Date.now
+    },
+    date:{
       type: String,
       required: true
-  },
-  userid:{
-    type: Schema.Types.ObjectId,
-    ref: 'user'
-  }
-
+    },
+    systolic: {
+        type: Number,
+        required: true
+    },
+    diastolic: {
+        type: Number,
+        required: true
+    },
+    notes: {
+        type: String,
+        required: true
+    },
+    userid:{
+      type: Schema.Types.ObjectId,
+      ref: 'user'
+    }
 })
 
 const AddBPStats = mongoose.model('AddBPStats' , AddBPStatsSchema);
