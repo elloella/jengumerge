d70bf5bd36b7a13f62a19fad70a612ff67c3c6f7
Ella Chambers <33899496+elloella@users.noreply.github.com>
2019-09-20 17:52:49 +0100

    Merge branch 'master' of https://github.com/chrisPhat/Jengu-

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MongoURI: 'mongodb+srv://ello_ella98:S0Uth23413@cluster0-supx3.mongodb.net/test?retryWrites=true' //Setting up the link with database
+  MongoURI: 'mongodb+srv://ello_ella98:S0Uth23413@cluster0-supx3.mongodb.net/jengumerge?retryWrites=true' //Setting up the link with database
 }
